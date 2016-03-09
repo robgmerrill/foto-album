49c74fe8a357f64d6909271ffcd6749e1d6b0b08
Rob Merrill <robgmerrill@gmail.com>
2016-03-09 03:45:13 -0800

    changed file structure to match a pattern of other classmates who have created a more level order of documents rather than splitting front and back end completely

--- models/foto.js
+++ models/foto.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 var fotoSchema = new mongoose.Schema({
   name: { type: String, default: 'Foto' },
   url: String,
-  summary: String,
+  summary: String
 });
 
 var Foto = module.exports = exports = mongoose.model('Foto', fotoSchema);
